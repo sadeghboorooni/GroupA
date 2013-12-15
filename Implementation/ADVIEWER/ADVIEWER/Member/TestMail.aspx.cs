6c5161ebddb61d92b2e37df7a1fbf8b824998159
hamidfalah <hamidfalah@gmail.com>
2013-12-15 21:36:34 +0000

    Email Advs First Commit

---
+++ Implementation/ADVIEWER/ADVIEWER/Member/TestMail.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Mail;
+
+namespace ADVIEWER.Member
+{
+    public partial class TestMail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var fromAddress = new MailAddress("adviewer.ir@gmail.com", "hamid");
+            var toAddress = new MailAddress("hamidfalah@gmail.com", "hamidfalah");
+            const string fromPassword = "hamid123";
+            const string subject = "testSubject";
+            const string body = "<h1>testBody</h1><br>testbody";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
